db3c0db505558b900f8dbceb180303293bf3fab9
hugosenari <hugosenari@gmail.com>
2022-03-12 04:26:52 -0300

    chore: re index nimeverything

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''everything  search engine wrapper'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimeverything-master.flake = false;
+  inputs.src-nimeverything-master.owner = "xland";
+  inputs.src-nimeverything-master.ref   = "refs/heads/master";
+  inputs.src-nimeverything-master.repo  = "nimeverything";
+  inputs.src-nimeverything-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimeverything-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nimeverything-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
