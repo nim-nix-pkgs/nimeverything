709789497c9bf46903848fe7ba74cee85796a58a
hugosenari <hugosenari@gmail.com>
2022-05-12 11:54:48 -0300

    chore: re index nimeverything

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."nimeverything-master".dir   = "master";
+  inputs."nimeverything-master".owner = "nim-nix-pkgs";
+  inputs."nimeverything-master".ref   = "master";
+  inputs."nimeverything-master".repo  = "nimeverything";
+  inputs."nimeverything-master".type  = "github";
+  inputs."nimeverything-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimeverything-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
